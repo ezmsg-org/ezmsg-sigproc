4a8231235853964d6dd4b7d738f54aeffd59e081
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-10-29 03:18:16 -0400

    slicer - Briefly convert labels to ndarray before slicing to accommodate different syntaxes.

--- tests/test_slicer.py
+++ tests/test_slicer.py
@@ -1,11 +1,29 @@
 import copy
+from dataclasses import dataclass, field
+import typing
 
 import numpy as np
 from ezmsg.util.messages.axisarray import AxisArray
+import pytest
 
 from ezmsg.sigproc.slicer import slicer, parse_slice
 
 from util import assert_messages_equal
+
+
+@dataclass
+class CustomAxis(AxisArray.Axis):
+    labels: typing.List[str] = field(default_factory=lambda: [])
+
+    @classmethod
+    def SpaceAxis(
+        cls, labels: typing.List[str]
+    ):  # , locs: typing.Optional[npt.NDArray] = None):
+        return cls(unit="mm", labels=labels)
+
+
+# Monkey-patch AxisArray with our customized Axis
+AxisArray.Axis = CustomAxis
 
 
 def test_parse_slice():
@@ -76,3 +94,31 @@ def test_slicer_gen_drop_dim():
     assert_messages_equal([msg_in], backup)
     assert msg_out.data.shape == (n_times,)
     assert np.array_equal(msg_out.data, msg_in.data[:, 5])
+
+
+@pytest.mark.parametrize("selection", [":3", "0, 1, 2"])
+def test_slicer_label(selection: str):
+    """
+    We use the monkey-patched AxisArray `labels` field that exists in several other ezmsg
+    modules that generate data.
+    """
+    n_times = 50
+    n_chans = 10
+    in_dat = np.arange(n_times * n_chans).reshape(n_times, n_chans)
+    msg_in = AxisArray(
+        in_dat,
+        dims=["time", "ch"],
+        axes={
+            "time": AxisArray.Axis.TimeAxis(fs=100.0, offset=0.1),
+            "ch": CustomAxis.SpaceAxis(labels=[str(_) for _ in range(n_chans)]),
+        },
+    )
+    backup = [copy.deepcopy(msg_in)]
+
+    gen = slicer(selection=selection, axis="ch")
+    # gen = slicer(selection=":3", axis="ch")
+    msg_out = gen.send(msg_in)
+    assert_messages_equal([msg_in], backup)
+    assert msg_out.data.shape == (n_times, 3)
+    assert np.array_equal(msg_out.data, msg_in.data[:, :3])
+    assert msg_out.axes["ch"].labels == msg_in.axes["ch"].labels[:3]
