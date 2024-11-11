17f5f4b5ac2de18a3a7c2b501161a17ab0cca9a4
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-11-10 23:49:46 -0500

    slicer - add support for CoordinateAxis

--- tests/test_slicer.py
+++ tests/test_slicer.py
@@ -1,6 +1,4 @@
 import copy
-from dataclasses import dataclass, field
-import typing
 
 import numpy as np
 from ezmsg.util.messages.axisarray import AxisArray
@@ -9,21 +7,6 @@ import pytest
 from ezmsg.sigproc.slicer import slicer, parse_slice
 
 from util import assert_messages_equal
-
-
-@dataclass
-class CustomAxis(AxisArray.Axis):
-    labels: typing.List[str] = field(default_factory=lambda: [])
-
-    @classmethod
-    def SpaceAxis(
-        cls, labels: typing.List[str]
-    ):  # , locs: typing.Optional[npt.NDArray] = None):
-        return cls(unit="mm", labels=labels)
-
-
-# Monkey-patch AxisArray with our customized Axis
-AxisArray.Axis = CustomAxis
 
 
 def test_parse_slice():
@@ -44,7 +27,17 @@ def test_slicer_generator():
     n_times = 13
     n_chans = 255
     in_dat = np.arange(n_times * n_chans).reshape(n_times, n_chans)
-    msg_in = AxisArray(in_dat, dims=["time", "ch"])
+    msg_in = AxisArray(
+        in_dat,
+        dims=["time", "ch"],
+        axes={
+            "time": AxisArray.Axis.TimeAxis(fs=100.0, offset=0.1),
+            "ch": AxisArray.CoordinateAxis(
+                data=np.array([f"Ch{_}" for _ in range(n_chans)]), dims=["ch"]
+            ),
+        },
+        key="test_slicer_generator",
+    )
     backup = [copy.deepcopy(msg_in)]
 
     gen = slicer(selection=":2", axis="ch")
@@ -85,7 +78,11 @@ def test_slicer_gen_drop_dim():
         dims=["time", "ch"],
         axes={
             "time": AxisArray.Axis.TimeAxis(fs=100.0, offset=0.1),
+            "ch": AxisArray.CoordinateAxis(
+                data=np.array([f"Ch{_}" for _ in range(n_chans)]), dims=["ch"]
+            ),
         },
+        key="test_slicer_gen_drop_dim",
     )
     backup = [copy.deepcopy(msg_in)]
 
@@ -96,7 +93,7 @@ def test_slicer_gen_drop_dim():
     assert np.array_equal(msg_out.data, msg_in.data[:, 5])
 
 
-@pytest.mark.parametrize("selection", [":3", "0, 1, 2"])
+@pytest.mark.parametrize("selection", [":3", "0, 1, 2", "Ch0, Ch1, Ch2"])
 def test_slicer_label(selection: str):
     """
     We use the monkey-patched AxisArray `labels` field that exists in several other ezmsg
@@ -110,8 +107,11 @@ def test_slicer_label(selection: str):
         dims=["time", "ch"],
         axes={
             "time": AxisArray.Axis.TimeAxis(fs=100.0, offset=0.1),
-            "ch": CustomAxis.SpaceAxis(labels=[str(_) for _ in range(n_chans)]),
+            "ch": AxisArray.CoordinateAxis(
+                data=np.array([f"Ch{_}" for _ in range(n_chans)]), dims=["ch"]
+            ),
         },
+        key="test_slicer_label",
     )
     backup = [copy.deepcopy(msg_in)]
 
@@ -121,4 +121,4 @@ def test_slicer_label(selection: str):
     assert_messages_equal([msg_in], backup)
     assert msg_out.data.shape == (n_times, 3)
     assert np.array_equal(msg_out.data, msg_in.data[:, :3])
-    assert msg_out.axes["ch"].labels == msg_in.axes["ch"].labels[:3]
+    assert np.array_equal(msg_out.axes["ch"].data, msg_in.axes["ch"].data[:3])
