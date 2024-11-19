384970ea7e957f7c60d5ef7e76a8853fa8b199a3
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-11-19 10:56:54 -0500

    Use pytest skipif instead of try...except

--- tests/test_scaler.py
+++ tests/test_scaler.py
@@ -2,9 +2,11 @@ import copy
 import os
 from typing import Optional, List
 from dataclasses import field
+import importlib.util
 
 import numpy as np
 from frozendict import frozendict
+import pytest
 import ezmsg.core as ez
 from ezmsg.util.messages.axisarray import AxisArray
 from ezmsg.util.terminate import TerminateOnTotalSettings, TerminateOnTotal
@@ -18,37 +20,35 @@ from ezmsg.sigproc.synth import Counter, CounterSettings
 from util import get_test_fn, assert_messages_equal
 
 
+@pytest.mark.skipif(
+    importlib.util.find_spec("river") is None, reason="requires `river` package"
+)
 def test_adaptive_standard_scaler_river():
-    try:
-        # Test data values taken from river:
-        # https://github.com/online-ml/river/blob/main/river/preprocessing/scale.py#L511-L536C17
-        data = np.array([5.278, 5.050, 6.550, 7.446, 9.472, 10.353, 11.784, 11.173])
-        expected_result = np.array(
-            [0.0, -0.816, 0.812, 0.695, 0.754, 0.598, 0.651, 0.124]
-        )
+    # Test data values taken from river:
+    # https://github.com/online-ml/river/blob/main/river/preprocessing/scale.py#L511-L536C17
+    data = np.array([5.278, 5.050, 6.550, 7.446, 9.472, 10.353, 11.784, 11.173])
+    expected_result = np.array([0.0, -0.816, 0.812, 0.695, 0.754, 0.598, 0.651, 0.124])
 
-        test_input = AxisArray(
-            np.tile(data, (2, 1)),
-            dims=["ch", "time"],
-            axes=frozendict({"time": AxisArray.TimeAxis(fs=100.0)}),
-        )
+    test_input = AxisArray(
+        np.tile(data, (2, 1)),
+        dims=["ch", "time"],
+        axes=frozendict({"time": AxisArray.TimeAxis(fs=100.0)}),
+    )
 
-        backup = [copy.deepcopy(test_input)]
+    backup = [copy.deepcopy(test_input)]
 
-        # The River example used alpha = 0.6
-        # tau = -gain / np.log(1 - alpha) and here we're using gain = 1.0
-        tau = 1.0914
-        _scaler = scaler(time_constant=tau, axis="time")
-        output = _scaler.send(test_input)
-        assert np.allclose(output.data[0], expected_result, atol=1e-3)
-        assert_messages_equal([test_input], backup)
+    # The River example used alpha = 0.6
+    # tau = -gain / np.log(1 - alpha) and here we're using gain = 1.0
+    tau = 1.0914
+    _scaler = scaler(time_constant=tau, axis="time")
+    output = _scaler.send(test_input)
+    assert np.allclose(output.data[0], expected_result, atol=1e-3)
+    assert_messages_equal([test_input], backup)
 
-        _scaler_np = scaler_np(time_constant=tau, axis="time")
-        output = _scaler_np.send(test_input)
-        assert np.allclose(output.data[0], expected_result, atol=1e-3)
-        assert_messages_equal([test_input], backup)
-    except ModuleNotFoundError:
-        pass  # Do not fail if river not installed.
+    _scaler_np = scaler_np(time_constant=tau, axis="time")
+    output = _scaler_np.send(test_input)
+    assert np.allclose(output.data[0], expected_result, atol=1e-3)
+    assert_messages_equal([test_input], backup)
 
 
 class ScalerTestSystemSettings(ez.Settings):
