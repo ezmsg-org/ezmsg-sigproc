4f1d6d62f904c5bda90dad7f1cac5b32054c5c2b
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-11-20 23:56:45 -0500

    Add EWMA class for faster and simpler ewma

--- src/ezmsg/sigproc/scaler.py
+++ src/ezmsg/sigproc/scaler.py
@@ -1,7 +1,9 @@
+import functools
 import typing
 
 import numpy as np
 import numpy.typing as npt
+import scipy.signal
 import ezmsg.core as ez
 from ezmsg.util.messages.axisarray import AxisArray
 from ezmsg.util.messages.util import replace
@@ -26,6 +28,138 @@ def _alpha_from_tau(tau: float, dt: float) -> float:
     :return: alpha, the "fading factor" in exponential smoothing.
     """
     return 1 - np.exp(-dt / tau)
+
+
+def ewma_step(
+    sample: npt.NDArray, zi: npt.NDArray, alpha, beta: typing.Optional[None] = None
+):
+    """
+    Do an exponentially weighted moving average step.
+
+    Args:
+        sample: The new sample.
+        zi: The output of the previous step.
+        alpha: Fading factor.
+        beta: Persisting factor. If None, it is calculated as 1-alpha.
+
+    Returns:
+        alpha * sample + beta * zi
+
+    """
+    # Potential micro-optimization:
+    #  Current: scalar-arr multiplication, scalar-arr multiplication, arr-arr addition
+    #  Alternative: arr-arr subtraction, arr-arr multiplication, arr-arr addition
+    # return zi + alpha * (new_sample - zi)
+    beta = beta or (1 - alpha)
+    return alpha * sample + beta * zi
+
+
+class EWMA:
+    def __init__(self, alpha: float):
+        self.beta = 1 - alpha
+        self._filt_func = functools.partial(
+            scipy.signal.lfilter, [alpha], [1.0, alpha - 1.0], axis=0
+        )
+        self.prev = None
+
+    def compute(self, arr: npt.NDArray) -> npt.NDArray:
+        if self.prev is None:
+            self.prev = self.beta * arr[:1]
+        expected, self.prev = self._filt_func(arr, zi=self.prev)
+        return expected
+
+
+class EWMA_Deprecated:
+    """
+    Grabbed these methods from https://stackoverflow.com/a/70998068 and other answers in that topic,
+    but they ended up being slower than the scipy.signal.lfilter method.
+    Additionally, `compute` and `compute2` suffer from potential errors as the vector length increases
+    and beta**n approaches zero.
+    """
+
+    def __init__(self, alpha: float, max_len: int):
+        self.alpha = alpha
+        self.beta = 1 - alpha
+        self.prev: typing.Optional[npt.NDArray] = None
+        self.weights = np.empty((max_len + 1,), float)
+        self._precalc_weights(max_len)
+        self._step_func = functools.partial(ewma_step, alpha=self.alpha, beta=self.beta)
+
+    def _precalc_weights(self, n: int):
+        #   (1-α)^0, (1-α)^1, (1-α)^2, ..., (1-α)^n
+        np.power(self.beta, np.arange(n + 1), out=self.weights)
+
+    def compute(
+        self, arr: npt.NDArray, out: typing.Optional[npt.NDArray] = None
+    ) -> npt.NDArray:
+        if out is None:
+            out = np.empty(arr.shape, arr.dtype)
+
+        n = arr.shape[0]
+        weights = self.weights[:n]
+        weights = np.expand_dims(weights, list(range(1, arr.ndim)))
+
+        #   α*P0, α*P1, α*P2, ..., α*Pn
+        np.multiply(self.alpha, arr, out)
+
+        #   α*P0/(1-α)^0, α*P1/(1-α)^1, α*P2/(1-α)^2, ..., α*Pn/(1-α)^n
+        np.divide(out, weights, out)
+
+        #   α*P0/(1-α)^0, α*P0/(1-α)^0 + α*P1/(1-α)^1, ...
+        np.cumsum(out, axis=0, out=out)
+
+        #   (α*P0/(1-α)^0)*(1-α)^0, (α*P0/(1-α)^0 + α*P1/(1-α)^1)*(1-α)^1, ...
+        np.multiply(out, weights, out)
+
+        # Add the previous output
+        if self.prev is None:
+            self.prev = arr[:1]
+
+        out += self.prev * np.expand_dims(
+            self.weights[1 : n + 1], list(range(1, arr.ndim))
+        )
+
+        self.prev = out[-1:]
+
+        return out
+
+    def compute2(self, arr: npt.NDArray) -> npt.NDArray:
+        """
+        Compute the Exponentially Weighted Moving Average (EWMA) of the input array.
+
+        Args:
+            arr: The input array to be smoothed.
+
+        Returns:
+            The smoothed array.
+        """
+        n = arr.shape[0]
+        if n > len(self.weights):
+            self._precalc_weights(n)
+        weights = self.weights[:n][::-1]
+        weights = np.expand_dims(weights, list(range(1, arr.ndim)))
+
+        result = np.cumsum(self.alpha * weights * arr, axis=0)
+        result = result / weights
+
+        # Handle the first call when prev is unset
+        if self.prev is None:
+            self.prev = arr[:1]
+
+        result += self.prev * np.expand_dims(
+            self.weights[1 : n + 1], list(range(1, arr.ndim))
+        )
+
+        # Store the result back into prev
+        self.prev = result[-1]
+
+        return result
+
+    def compute_sample(self, new_sample: npt.NDArray) -> npt.NDArray:
+        if self.prev is None:
+            self.prev = new_sample
+        self.prev = self._step_func(new_sample, self.prev)
+        return self.prev
 
 
 @consumer
@@ -96,10 +230,8 @@ def scaler_np(
     msg_out = AxisArray(np.array([]), dims=[""])
 
     # State variables
-    alpha: float = 0.0
-    means: typing.Optional[npt.NDArray] = None
-    vars_means: typing.Optional[npt.NDArray] = None
-    vars_sq_means: typing.Optional[npt.NDArray] = None
+    samps_ewma: typing.Optional[EWMA] = None
+    vars_sq_ewma: typing.Optional[EWMA] = None
 
     # Reset if input changes
     check_input = {
@@ -108,45 +240,32 @@ def scaler_np(
         "key": None,  # Key change implies buffered means/vars are invalid.
     }
 
-    def _ew_update(arr, prev, _alpha):
-        if np.all(prev == 0):
-            return arr
-        # return _alpha * arr + (1 - _alpha) * prev
-        # Micro-optimization: sub, mult, add (below) is faster than sub, mult, mult, add (above)
-        return prev + _alpha * (arr - prev)
-
     while True:
         msg_in: AxisArray = yield msg_out
 
         axis = axis or msg_in.dims[0]
         axis_idx = msg_in.get_axis_idx(axis)
 
-        if msg_in.axes[axis].gain != check_input["gain"]:
-            alpha = _alpha_from_tau(time_constant, msg_in.axes[axis].gain)
-            check_input["gain"] = msg_in.axes[axis].gain
-
         data: npt.NDArray = np.moveaxis(msg_in.data, axis_idx, 0)
         b_reset = data.shape[1:] != check_input["shape"]
-        b_reset |= msg_in.key != check_input["key"]
+        b_reset = b_reset or msg_in.axes[axis].gain != check_input["gain"]
+        b_reset = b_reset or msg_in.key != check_input["key"]
         if b_reset:
             check_input["shape"] = data.shape[1:]
+            check_input["gain"] = msg_in.axes[axis].gain
             check_input["key"] = msg_in.key
-            vars_sq_means = np.zeros_like(data[0], dtype=float)
-            vars_means = np.zeros_like(data[0], dtype=float)
-            means = np.zeros_like(data[0], dtype=float)
+            alpha = _alpha_from_tau(time_constant, msg_in.axes[axis].gain)
+            samps_ewma = EWMA(alpha=alpha)
+            vars_sq_ewma = EWMA(alpha=alpha)
 
-        result = np.zeros_like(data)
-        for sample_ix in range(data.shape[0]):
-            sample = data[sample_ix]
-            # Update step
-            vars_means = _ew_update(sample, vars_means, alpha)
-            vars_sq_means = _ew_update(sample**2, vars_sq_means, alpha)
-            means = _ew_update(sample, means, alpha)
-            # Get step
-            varis = vars_sq_means - vars_means**2
-            y = (sample - means) / (varis**0.5)
-            result[sample_ix] = y
+        # Update step
+        means = samps_ewma.compute(data)
+        vars_sq_means = vars_sq_ewma.compute(data**2)
 
+        # Get step
+        varis = vars_sq_means - means**2
+        with np.errstate(divide="ignore", invalid="ignore"):
+            result = (data - means) / (varis**0.5)
         result[np.isnan(result)] = 0.0
         result = np.moveaxis(result, 0, axis_idx)
         msg_out = replace(msg_in, data=result)
