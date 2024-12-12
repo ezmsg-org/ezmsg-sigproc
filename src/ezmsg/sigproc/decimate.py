25abf521ce3bec17bab57783b33794e5a9d2fe1d
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-12-12 01:08:09 -0500

    Decimate: replace `factor` arg with `target_rate`

--- src/ezmsg/sigproc/decimate.py
+++ src/ezmsg/sigproc/decimate.py
@@ -1,8 +1,31 @@
+import typing
+
 import ezmsg.core as ez
 from ezmsg.util.messages.axisarray import AxisArray
 
 from .cheby import ChebyshevFilter, ChebyshevFilterSettings
 from .downsample import Downsample, DownsampleSettings
+from .filter import FilterCoefsMultiType
+
+
+class ChebyForDecimate(ChebyshevFilter):
+    """
+    A :obj:`ChebyshevFilter` node with a design filter method that additionally accepts a target sampling rate,
+     and if the target rate cannot be achieved it returns None, else it returns the filter coefficients.
+    """
+
+    def design_filter(
+        self,
+    ) -> typing.Callable[[float], FilterCoefsMultiType | None]:
+        def cheby_opt_design_fun(fs: float) -> FilterCoefsMultiType | None:
+            if fs is None:
+                return None
+            ds_factor = int(fs / (2.5 * self.SETTINGS.Wn))
+            if ds_factor < 2:
+                return None
+            partial_fun = super(ChebyForDecimate, self).design_filter()
+            return partial_fun(fs)
+        return cheby_opt_design_fun
 
 
 class Decimate(ez.Collection):
@@ -16,17 +39,18 @@ class Decimate(ez.Collection):
     INPUT_SIGNAL = ez.InputStream(AxisArray)
     OUTPUT_SIGNAL = ez.OutputStream(AxisArray)
 
-    FILTER = ChebyshevFilter()
+    FILTER = ChebyForDecimate()
     DOWNSAMPLE = Downsample()
 
     def configure(self) -> None:
+
         cheby_settings = ChebyshevFilterSettings(
-            order=8 if self.SETTINGS.factor > 1 else 0,
+            order=8,
             ripple_tol=0.05,
-            Wn=0.8 / self.SETTINGS.factor if self.SETTINGS.factor > 1 else None,
+            Wn=0.4 * self.SETTINGS.target_rate,
             btype="lowpass",
             axis=self.SETTINGS.axis,
-            wn_hz=False,
+            wn_hz=True,
         )
         self.FILTER.apply_settings(cheby_settings)
         self.DOWNSAMPLE.apply_settings(self.SETTINGS)
