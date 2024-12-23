50b95c05f54ce48cc8eaa8f47fb2ca0444537e11
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-12-23 17:05:56 -0500

    Expose `windowing` `anchor` argument through spectrogram gen func and unit.

--- src/ezmsg/sigproc/spectrogram.py
+++ src/ezmsg/sigproc/spectrogram.py
@@ -5,7 +5,7 @@ from ezmsg.util.messages.axisarray import AxisArray
 from ezmsg.util.generator import consumer, compose
 from ezmsg.util.messages.modify import modify_axis
 
-from .window import windowing
+from .window import windowing, Anchor
 from .spectrum import spectrum, WindowFunction, SpectralTransform, SpectralOutput
 from .base import GenAxisArray
 
@@ -14,6 +14,7 @@ from .base import GenAxisArray
 def spectrogram(
     window_dur: float | None = None,
     window_shift: float | None = None,
+    window_anchor: str | Anchor = Anchor.BEGINNING,
     window: WindowFunction = WindowFunction.HANNING,
     transform: SpectralTransform = SpectralTransform.REL_DB,
     output: SpectralOutput = SpectralOutput.POSITIVE,
@@ -28,6 +29,7 @@ def spectrogram(
     Args:
         window_dur: See :obj:`ezmsg.sigproc.window.windowing`
         window_shift: See :obj:`ezmsg.sigproc.window.windowing`
+        window_anchor: See :obj:`ezmsg.sigproc.window.windowing`
         window: See :obj:`ezmsg.sigproc.spectrum.spectrum`
         transform: See :obj:`ezmsg.sigproc.spectrum.spectrum`
         output: See :obj:`ezmsg.sigproc.spectrum.spectrum`
@@ -44,6 +46,7 @@ def spectrogram(
             window_dur=window_dur,
             window_shift=window_shift,
             zero_pad_until="shift" if window_shift is not None else "input",
+            anchor=window_anchor,
         ),
         spectrum(axis="time", window=window, transform=transform, output=output),
         modify_axis(name_map={"win": "time"}),
@@ -66,6 +69,7 @@ class SpectrogramSettings(ez.Settings):
     window_dur: float | None = None  # window duration in seconds
     window_shift: float | None = None
     """"window step in seconds. If None, window_shift == window_dur"""
+    window_anchor: str | Anchor = Anchor.BEGINNING
 
     # See SpectrumSettings for details of following settings:
     window: WindowFunction = WindowFunction.HAMMING
@@ -84,6 +88,7 @@ class Spectrogram(GenAxisArray):
         self.STATE.gen = spectrogram(
             window_dur=self.SETTINGS.window_dur,
             window_shift=self.SETTINGS.window_shift,
+            window_anchor=self.SETTINGS.window_anchor,
             window=self.SETTINGS.window,
             transform=self.SETTINGS.transform,
             output=self.SETTINGS.output,
