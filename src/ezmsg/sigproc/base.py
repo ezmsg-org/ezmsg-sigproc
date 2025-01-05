2f1df1f6ac5f18b2df07dc24cf9424c70e4085d4
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-01-05 18:10:27 -0500

    First attempt at using protocols and generic non-unit base classes for signal transformers.

--- src/ezmsg/sigproc/base.py
+++ src/ezmsg/sigproc/base.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 import math
 import traceback
 import typing
@@ -7,6 +8,127 @@ from ezmsg.util.messages.axisarray import AxisArray
 from ezmsg.util.generator import GenState
 
 from .util.profile import profile_subpub
+
+
+# Type variables
+SettingsType = typing.TypeVar("SettingsType", bound=ez.Settings)
+MessageType = typing.TypeVar("MessageType")
+StateType = typing.TypeVar("StateType", bound=ez.State)
+
+
+class SignalTransformer(typing.Protocol[StateType, MessageType, SettingsType]):
+    def check_metadata(self, message: MessageType) -> bool: ...
+
+    def reset(self, message: MessageType) -> None: ...
+
+    @property
+    def state(self) -> StateType: ...
+
+    @state.setter
+    def state(self, state: StateType) -> None: ...
+
+    def _process(self, message: MessageType) -> MessageType: ...
+
+    def transform(self, message: MessageType) -> MessageType: ...
+
+    @staticmethod
+    def stateful_op(state: StateType, message: MessageType) -> tuple[StateType, MessageType]: ...
+
+
+class BaseSignalTransformer(ABC, typing.Generic[StateType, MessageType, SettingsType]):
+    """
+    Abstract base class implementing common transformer functionality.
+
+    Create a concrete transformer by subclassing this class and implementing the abstract methods.
+    Additionally, set the class variable `state_type` to the State class that the transformer will use.
+    e.g. `state_type = MyStateClass`
+    """
+
+    state_type: typing.Type[StateType]
+
+    def __init__(self, settings: SettingsType):
+        self.settings = settings
+        self._state: StateType = self.state_type()
+
+    @abstractmethod
+    def check_metadata(self, message: MessageType) -> bool:
+        ...
+
+    @abstractmethod
+    def reset(self, message: MessageType) -> None:
+        ...
+
+    @property
+    def state(self) -> StateType:
+        return self._state
+
+    @state.setter
+    def state(self, state: StateType) -> None:
+        if state is not None:
+            self._state = state
+
+    @abstractmethod
+    def _process(self, message: MessageType) -> MessageType:
+        ...
+
+    def transform(self, message: MessageType) -> MessageType:
+        if self.check_metadata(message):
+            self.reset(message)
+
+        return self._process(message)
+
+    def stateful_op(self, state: StateType, message: MessageType) -> tuple[StateType, MessageType]:
+        self.state = state
+        result = self.transform(message)
+        return self.state, result
+
+
+class BaseSignalTransformerUnit(ez.Unit, typing.Generic[StateType, MessageType, SettingsType]):
+    """
+    Implement a new Unit as follows:
+
+    class CustomUnit(BaseSignalTransformerUnit[
+        CustomTransformerState        # StateType
+        CustomTransformerSettings,    # SettingsType
+        AxisArray,                    # MessageType
+    ]):
+        SETTINGS = CustomTransformerSettings
+        transformer_type = CustomTransformer
+
+    ... that's all!
+
+    Where CustomTransformerState, CustomTransformerSettings, and CustomTransformer
+    are custom implementations of ez.State, ez.Settings, and BaseSignalTransformer, respectively.
+    """
+    INPUT_SIGNAL = ez.InputStream(MessageType)
+    OUTPUT_SIGNAL = ez.OutputStream(MessageType)
+    INPUT_SETTINGS = ez.InputStream(SettingsType)
+
+    # Class variable that concrete classes will override
+    transformer_type: typing.Type[SignalTransformer[StateType, MessageType, SettingsType]]
+
+    async def initialize(self) -> None:
+        self.transformer = self.create_transformer()
+
+    def create_transformer(self) -> SignalTransformer[StateType, MessageType, SettingsType]:
+        """Create the transformer instance from settings."""
+        # return self.transformer_type(**dataclasses.asdict(self.SETTINGS))
+        return self.transformer_type(settings=self.SETTINGS)
+
+    @ez.subscriber(INPUT_SETTINGS)
+    async def on_settings(self, msg: SettingsType) -> None:
+        self.apply_settings(msg)
+        self.create_transformer()
+
+    @ez.subscriber(INPUT_SIGNAL, zero_copy=True)
+    @ez.publisher(OUTPUT_SIGNAL)
+    async def on_signal(self, message: MessageType) -> typing.AsyncGenerator:
+        try:
+            ret = self.transformer.transform(message)
+            if math.prod(ret.data.shape) > 0:
+                yield self.OUTPUT_SIGNAL, ret
+        except Exception:
+            ez.logger.info(traceback.format_exc())
 
 
 class GenAxisArray(ez.Unit):
--- src/ezmsg/sigproc/downsample.py
+++ src/ezmsg/sigproc/downsample.py
@@ -1,4 +1,5 @@
 import typing
+from math import factorial
 
 import numpy as np
 from ezmsg.util.messages.axisarray import (
@@ -9,8 +10,120 @@ from ezmsg.util.messages.axisarray import (
 from ezmsg.util.generator import consumer
 import ezmsg.core as ez
 
-from .base import GenAxisArray
+from .base import BaseSignalTransformer, BaseSignalTransformerUnit
 
+
+class DownsampleSettings(ez.Settings):
+    """
+    Settings for :obj:`Downsample` node.
+    See :obj:`downsample` documentation for a description of the parameters.
+    """
+
+    axis: str = "time"
+    """The name of the axis along which to downsample."""
+
+    target_rate: float | None = None
+    """Desired rate after downsampling. The actual rate will be the nearest integer factor of the
+            input rate that is the same or higher than the target rate."""
+
+    factor: int | None = None
+    """Explicitly specify downsample factor.  If specified, target_rate is ignored."""
+
+
+class DownsampleState(ez.State):
+    q: int = 0
+    """The integer downsampling factor. It will be determined based on the target rate."""
+
+    s_idx: int = 0
+    """Index of the next msg's first sample into the virtual rotating ds_factor counter."""
+
+    hash: int = 0
+
+
+class DownsampleTransformer(BaseSignalTransformer[DownsampleState, AxisArray, DownsampleSettings]):
+    """
+    Construct a generator that yields a downsampled version of the data .send() to it.
+    Downsampled data simply comprise every `factor`th sample.
+    This should only be used following appropriate lowpass filtering.
+    If your pipeline does not already have lowpass filtering then consider
+    using the :obj:`Decimate` collection instead.
+
+    Args:
+        axis: The name of the axis along which to downsample.
+            Note: The axis must exist in the message .axes and be of type AxisArray.LinearAxis.
+        target_rate: Desired rate after downsampling. The actual rate will be the nearest integer factor of the
+            input rate that is the same or higher than the target rate.
+
+    Returns:
+        A primed generator object ready to receive an :obj:`AxisArray` via `.send(axis_array)`
+        and yields an :obj:`AxisArray` with its data downsampled.
+        Note that if a send chunk does not have sufficient samples to reach the
+        next downsample interval then an :obj:`AxisArray` with size-zero data is yielded.
+    """
+    state_type = DownsampleState
+
+    def check_metadata(self, message: AxisArray) -> bool:
+        return self.state.hash != hash((message.axes[self.settings.axis].gain, message.key))
+
+    def reset(self, message: AxisArray) -> None:
+        axis_info = message.get_axis(self.settings.axis)
+
+        if self.settings.factor is not None:
+            q = self.settings.factor
+        elif self.settings.target_rate is None:
+            q = 1
+        else:
+            q = int(1 / (axis_info.gain * self.settings.target_rate))
+        if q < 1:
+            ez.logger.warning(
+                f"Target rate {self.settings.target_rate} cannot be achieved with input rate of {1 / axis_info.gain}."
+                "Setting factor to 1."
+            )
+            q = 1
+        self._state.q = q
+        self._state.s_idx = 0
+        self._state.hash = hash((axis_info.gain, message.key))
+
+    def _process(self, message: AxisArray) -> AxisArray:
+        axis = self.settings.axis
+        axis_info = message.get_axis(axis)
+        axis_idx = message.get_axis_idx(axis)
+
+        n_samples = message.data.shape[axis_idx]
+        samples = np.arange(self._state.s_idx, self.state.s_idx + n_samples) % self._state.q
+        if n_samples > 0:
+            # Update state for next iteration.
+            self._state.s_idx = samples[-1] + 1
+
+        pub_samples = np.where(samples == 0)[0]
+        if len(pub_samples) > 0:
+            n_step = pub_samples[0].item()
+            data_slice = pub_samples
+        else:
+            n_step = 0
+            data_slice = slice(None, 0, None)
+        msg_out = replace(
+            message,
+            data=slice_along_axis(message.data, data_slice, axis=axis_idx),
+            axes={
+                **message.axes,
+                axis: replace(
+                    axis_info,
+                    gain=axis_info.gain * self._state.q,
+                    offset=axis_info.offset + axis_info.gain * n_step,
+                ),
+            },
+        )
+        return msg_out
+
+
+class Downsample(BaseSignalTransformerUnit[DownsampleState, DownsampleSettings, AxisArray]):
+    SETTINGS = DownsampleSettings
+    transformer_type = DownsampleTransformer
+
+
+# TODO: downsample = lambda axis=None, target_rate=None: iter(DownsampleTransformer(DownsampleSettings(axis=axis, target_rate=target_rate)))
+#  This will require __iter__ and send methods on BaseSignalTransformer
 
 @consumer
 def downsample(
@@ -39,88 +152,8 @@ def downsample(
     """
     msg_out = AxisArray(np.array([]), dims=[""])
 
-    # state variables
-    q: int = 0  # The integer downsampling factor. It will be determined based on the target rate.
-    s_idx: int = 0  # Index of the next msg's first sample into the virtual rotating ds_factor counter.
-
-    check_input = {"gain": None, "key": None}
+    _tx = DownsampleTransformer(DownsampleSettings(axis=axis, target_rate=target_rate, factor=factor))
 
     while True:
         msg_in: AxisArray = yield msg_out
-
-        if axis is None:
-            axis = msg_in.dims[0]
-        axis_info = msg_in.get_axis(axis)
-        axis_idx = msg_in.get_axis_idx(axis)
-
-        b_reset = (
-            msg_in.axes[axis].gain != check_input["gain"]
-            or msg_in.key != check_input["key"]
-        )
-        if b_reset:
-            check_input["gain"] = axis_info.gain
-            check_input["key"] = msg_in.key
-            # Reset state variables
-            s_idx = 0
-            if factor is not None:
-                q = factor
-            elif target_rate is None:
-                q = 1
-            else:
-                q = int(1 / (axis_info.gain * target_rate))
-            if q < 1:
-                ez.logger.warning(
-                    f"Target rate {target_rate} cannot be achieved with input rate of {1/axis_info.gain}."
-                    "Setting factor to 1."
-                )
-                q = 1
-
-        n_samples = msg_in.data.shape[axis_idx]
-        samples = np.arange(s_idx, s_idx + n_samples) % q
-        if n_samples > 0:
-            # Update state for next iteration.
-            s_idx = samples[-1] + 1
-
-        pub_samples = np.where(samples == 0)[0]
-        if len(pub_samples) > 0:
-            n_step = pub_samples[0].item()
-            data_slice = pub_samples
-        else:
-            n_step = 0
-            data_slice = slice(None, 0, None)
-        msg_out = replace(
-            msg_in,
-            data=slice_along_axis(msg_in.data, data_slice, axis=axis_idx),
-            axes={
-                **msg_in.axes,
-                axis: replace(
-                    axis_info,
-                    gain=axis_info.gain * q,
-                    offset=axis_info.offset + axis_info.gain * n_step,
-                ),
-            },
-        )
-
-
-class DownsampleSettings(ez.Settings):
-    """
-    Settings for :obj:`Downsample` node.
-    See :obj:`downsample` documentation for a description of the parameters.
-    """
-
-    axis: str | None = None
-    target_rate: float | None = None
-    factor: int | None = None
-
-
-class Downsample(GenAxisArray):
-    """:obj:`Unit` for :obj:`bandpower`."""
-
-    SETTINGS = DownsampleSettings
-
-    def construct_generator(self):
-        self.STATE.gen = downsample(
-            axis=self.SETTINGS.axis,
-            target_rate=self.SETTINGS.target_rate,
-            factor=self.SETTINGS.factor,
-        )
+        msg_out = _tx.transform(msg_in)
