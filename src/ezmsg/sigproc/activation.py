f2eea60afd4fde5c9aa8e02b299a13e44f44bb0b
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-02-15 11:27:20 -0500

    refactor activation generator as trasnformer.

--- src/ezmsg/sigproc/activation.py
+++ src/ezmsg/sigproc/activation.py
@@ -1,14 +1,10 @@
-import typing
-
-import numpy as np
 import scipy.special
 import ezmsg.core as ez
 from ezmsg.util.messages.axisarray import AxisArray
 from ezmsg.util.messages.util import replace
-from ezmsg.util.generator import consumer
 
 from .spectral import OptionsEnum
-from .base import GenAxisArray
+from .base import BaseTransformer, BaseTransformerUnit
 
 
 class ActivationFunction(OptionsEnum):
@@ -39,10 +35,39 @@ ACTIVATIONS = {
 }
 
 
-@consumer
+class ActivationSettings(ez.Settings):
+    function: str | ActivationFunction = ActivationFunction.NONE
+    """An enum value from ActivationFunction or a string representing the activation function.
+         Possible values are: SIGMOID, EXPIT, LOGIT, LOGEXPIT, "sigmoid", "expit", "logit", "log_expit".
+         SIGMOID and EXPIT are equivalent. See :obj:`scipy.special.expit` for more details."""
+
+
+class ActivationTransformer(BaseTransformer[ActivationSettings, AxisArray]):
+    def _process(self, message: AxisArray) -> AxisArray:
+        if type(self.settings.function) is ActivationFunction:
+            func = ACTIVATIONS[self.settings.function]
+        else:
+            # str type handling
+            function = self.settings.function.lower()
+            if function not in ActivationFunction.options():
+                raise ValueError(
+                    f"Unrecognized activation function {function}. Must be one of {ACTIVATIONS.keys()}"
+                )
+            function = list(ACTIVATIONS.keys())[
+                ActivationFunction.options().index(function)
+            ]
+            func = ACTIVATIONS[function]
+
+        return replace(message, data=func(message.data))
+
+
+class Activation(BaseTransformerUnit[ActivationSettings, AxisArray, ActivationTransformer]):
+    SETTINGS = ActivationSettings
+
+
 def activation(
     function: str | ActivationFunction,
-) -> typing.Generator[AxisArray, AxisArray, None]:
+) -> ActivationTransformer:
     """
     Transform the data with a simple activation function.
 
@@ -51,37 +76,7 @@ def activation(
          Possible values are: SIGMOID, EXPIT, LOGIT, LOGEXPIT, "sigmoid", "expit", "logit", "log_expit".
          SIGMOID and EXPIT are equivalent. See :obj:`scipy.special.expit` for more details.
 
-    Returns: A primed generator that, when passed an input message via `.send(msg)`, yields an AxisArray
-     with the data payload containing a transformed version of the input data.
+    Returns: :obj:`ActivationTransformer`
 
     """
-    if type(function) is ActivationFunction:
-        func = ACTIVATIONS[function]
-    else:
-        # str type. There's probably an easier way to support either enum or str argument. Oh well this works.
-        function: str = function.lower()
-        if function not in ActivationFunction.options():
-            raise ValueError(
-                f"Unrecognized activation function {function}. Must be one of {ACTIVATIONS.keys()}"
-            )
-        function = list(ACTIVATIONS.keys())[
-            ActivationFunction.options().index(function)
-        ]
-        func = ACTIVATIONS[function]
-
-    msg_out = AxisArray(np.array([]), dims=[""])
-
-    while True:
-        msg_in: AxisArray = yield msg_out
-        msg_out = replace(msg_in, data=func(msg_in.data))
-
-
-class ActivationSettings(ez.Settings):
-    function: str = ActivationFunction.NONE
-
-
-class Activation(GenAxisArray):
-    SETTINGS = ActivationSettings
-
-    def construct_generator(self):
-        self.STATE.gen = activation(function=self.SETTINGS.function)
+    return ActivationTransformer(ActivationSettings(function=function))
