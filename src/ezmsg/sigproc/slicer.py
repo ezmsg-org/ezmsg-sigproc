5a0756b1ec4930c2a6538b71852e02d0e79f1468
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-02-15 16:14:24 -0500

    Refactor `slicer` generator to BaseStatefulTransformer

--- src/ezmsg/sigproc/slicer.py
+++ src/ezmsg/sigproc/slicer.py
@@ -11,13 +11,11 @@ from ezmsg.util.messages.axisarray import (
 )
 from ezmsg.util.generator import consumer
 
-from .base import GenAxisArray
-
+from .base import GenAxisArray, ProcessorState, BaseStatefulTransformer, BaseTransformerUnit
 
 """
 Slicer:Select a subset of data along a particular axis.
 """
-
 
 def parse_slice(
     s: str,
@@ -61,10 +59,85 @@ def parse_slice(
     return tuple([item for sublist in suplist for item in sublist])
 
 
-@consumer
+class SlicerSettings(ez.Settings):
+    selection: str = ""
+    """selection: See :obj:`ezmsg.sigproc.slicer.parse_slice` for details."""
+
+    axis: str | None = None
+    """The name of the axis to slice along. If None, the last axis is used."""
+
+
+class SlicerState(ProcessorState):
+    slice: slice | npt.NDArray | None = None
+    new_axis: AxisBase | None = None
+    b_change_dims: bool = False
+
+
+class SlicerTransformer(BaseStatefulTransformer[SlicerSettings, AxisArray, SlicerState]):
+    def _hash_message(self, message: AxisArray) -> int:
+        axis = self.settings.axis or message.dims[-1]
+        axis_idx = message.get_axis_idx(axis)
+        return hash((message.key, message.data.shape[axis_idx]))
+
+    def _reset_state(self, message: AxisArray) -> None:
+        axis = self.settings.axis or message.dims[-1]
+        axis_idx = message.get_axis_idx(axis)
+        self._state.new_axis = None
+        self._state.b_change_dims = False
+
+        # Calculate the slice
+        _slices = parse_slice(self.settings.selection, message.axes.get(axis, None))
+        if len(_slices) == 1:
+            self._state.slice = _slices[0]
+            self._state.b_change_dims = isinstance(self._state.slice, int)
+        else:
+            indices = np.arange(message.data.shape[axis_idx])
+            indices = np.hstack([indices[_] for _ in _slices])
+            self._state.slice = np.s_[indices]
+
+        # Create the output axis
+        if (
+            axis in message.axes
+            and hasattr(message.axes[axis], "data")
+            and len(message.axes[axis].data) > 0
+        ):
+            in_data = np.array(message.axes[axis].data)
+            if self._state.b_change_dims:
+                out_data = in_data[self._state.slice : self._state.slice + 1]
+            else:
+                out_data = in_data[self._state.slice]
+            self._state.new_axis = replace(message.axes[axis], data=out_data)
+
+    def _process(self, message: AxisArray) -> AxisArray:
+        axis = self.settings.axis or message.dims[-1]
+        axis_idx = message.get_axis_idx(axis)
+
+        replace_kwargs = {}
+        if self._state.b_change_dims:
+            replace_kwargs["dims"] = [
+                _ for dim_ix, _ in enumerate(message.dims) if dim_ix != axis_idx
+            ]
+            replace_kwargs["axes"] = {k: v for k, v in message.axes.items() if k != axis}
+        elif self._state.new_axis is not None:
+            replace_kwargs["axes"] = {
+                k: (v if k != axis else self._state.new_axis)
+                for k, v in message.axes.items()
+            }
+
+        return replace(
+            message,
+            data=slice_along_axis(message.data, self._state.slice, axis_idx),
+            **replace_kwargs,
+        )
+
+
+class Slicer(BaseTransformerUnit[SlicerSettings, AxisArray, SlicerTransformer]):
+    SETTINGS = SlicerSettings
+
+
 def slicer(
     selection: str = "", axis: str | None = None
-) -> typing.Generator[AxisArray, AxisArray, None]:
+) -> SlicerTransformer:
     """
     Slice along a particular axis.
 
@@ -73,94 +146,6 @@ def slicer(
         axis: The name of the axis to slice along. If None, the last axis is used.
 
     Returns:
-        A primed generator object ready to yield an :obj:`AxisArray` for each .send(axis_array)
-        with the data payload containing a sliced view of the input data.
-
+        :obj:`SlicerTransformer`
     """
-    msg_out = AxisArray(np.array([]), dims=[""])
-
-    # State variables
-    _slice: slice | npt.NDArray | None = None
-    new_axis: AxisBase | None = None
-    b_change_dims: bool = False  # If number of dimensions changes when slicing
-
-    # Reset if input changes
-    check_input = {
-        "key": None,  # key change used as proxy for label change, which we don't check explicitly
-        "len": None,
-    }
-
-    while True:
-        msg_in: AxisArray = yield msg_out
-
-        axis = axis or msg_in.dims[-1]
-        axis_idx = msg_in.get_axis_idx(axis)
-
-        b_reset = _slice is None  # or new_axis is None
-        b_reset = b_reset or msg_in.key != check_input["key"]
-        b_reset = b_reset or (
-            (msg_in.data.shape[axis_idx] != check_input["len"])
-            and (type(_slice) is np.ndarray)
-        )
-        if b_reset:
-            check_input["key"] = msg_in.key
-            check_input["len"] = msg_in.data.shape[axis_idx]
-            new_axis = None  # Will hold updated metadata
-            b_change_dims = False
-
-            # Calculate the slice
-            _slices = parse_slice(selection, msg_in.axes.get(axis, None))
-            if len(_slices) == 1:
-                _slice = _slices[0]
-                # Do we drop the sliced dimension?
-                b_change_dims = isinstance(_slice, int)
-            else:
-                # Multiple slices, but this cannot be done in a single step, so we convert the slices
-                #  to a discontinuous set of integer indexes.
-                indices = np.arange(msg_in.data.shape[axis_idx])
-                indices = np.hstack([indices[_] for _ in _slices])
-                _slice = np.s_[indices]  # Integer scalar array
-
-            # Create the output axis.
-            if (
-                axis in msg_in.axes
-                and hasattr(msg_in.axes[axis], "data")
-                and len(msg_in.axes[axis].data) > 0
-            ):
-                in_data = np.array(msg_in.axes[axis].data)
-                if b_change_dims:
-                    out_data = in_data[_slice : _slice + 1]
-                else:
-                    out_data = in_data[_slice]
-                new_axis = replace(msg_in.axes[axis], data=out_data)
-
-        replace_kwargs = {}
-        if b_change_dims:
-            # Dropping the target axis
-            replace_kwargs["dims"] = [
-                _ for dim_ix, _ in enumerate(msg_in.dims) if dim_ix != axis_idx
-            ]
-            replace_kwargs["axes"] = {k: v for k, v in msg_in.axes.items() if k != axis}
-        elif new_axis is not None:
-            replace_kwargs["axes"] = {
-                k: (v if k != axis else new_axis) for k, v in msg_in.axes.items()
-            }
-        msg_out = replace(
-            msg_in,
-            data=slice_along_axis(msg_in.data, _slice, axis_idx),
-            **replace_kwargs,
-        )
-
-
-class SlicerSettings(ez.Settings):
-    selection: str = ""
-    axis: str | None = None
-
-
-class Slicer(GenAxisArray):
-    SETTINGS = SlicerSettings
-
-    def construct_generator(self):
-        self.STATE.gen = slicer(
-            selection=self.SETTINGS.selection, axis=self.SETTINGS.axis
-        )
+    return SlicerTransformer(SlicerSettings(selection=selection, axis=axis))
