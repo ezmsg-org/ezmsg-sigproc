3c019f8235094c3c62c51c99941c75b0f6994ef3
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-02-15 11:42:38 -0500

    refactor affine_transform from generator to Transformer

--- src/ezmsg/sigproc/affinetransform.py
+++ src/ezmsg/sigproc/affinetransform.py
@@ -1,4 +1,5 @@
 import os
+from dataclasses import field
 from pathlib import Path
 import typing
 
@@ -9,122 +10,12 @@ from ezmsg.util.messages.axisarray import AxisArray, AxisBase
 from ezmsg.util.messages.util import replace
 from ezmsg.util.generator import consumer
 
-from .base import GenAxisArray
-
-
-@consumer
-def affine_transform(
-    weights: np.ndarray | str | Path,
-    axis: str | None = None,
-    right_multiply: bool = True,
-) -> typing.Generator[AxisArray, AxisArray, None]:
-    """
-    Perform affine transformations on streaming data.
-
-    Args:
-        weights: An array of weights or a path to a file with weights compatible with np.loadtxt.
-        axis: The name of the axis to apply the transformation to. Defaults to the leading (0th) axis in the array.
-        right_multiply: Set False to transpose the weights before applying.
-
-    Returns:
-        A primed generator object that yields an :obj:`AxisArray` object for every
-        :obj:`AxisArray` it receives via `send`.
-    """
-    msg_out = AxisArray(np.array([]), dims=[""])
-
-    # Check parameters
-    if isinstance(weights, str):
-        if weights == "passthrough":
-            weights = None
-        else:
-            weights = Path(os.path.abspath(os.path.expanduser(weights)))
-    if isinstance(weights, Path):
-        weights = np.loadtxt(weights, delimiter=",")
-    if not right_multiply:
-        weights = weights.T
-    if weights is not None:
-        weights = np.ascontiguousarray(weights)
-
-    # State variables
-    # New axis with transformed labels, if required
-    new_axis: AxisBase | None = None
-
-    # Reset if any of these change.
-    check_input = {"key": None}
-    # We assume key change catches labels change; we don't want to check labels every message
-    # We don't need to check if input size has changed because weights multiplication will fail if so.
-
-    while True:
-        msg_in: AxisArray = yield msg_out
-
-        if weights is None:
-            msg_out = msg_in
-            continue
-
-        axis = axis or msg_in.dims[-1]  # Note: Most nodes default do dim[0]
-        axis_idx = msg_in.get_axis_idx(axis)
-
-        b_reset = msg_in.key != check_input["key"]
-        if b_reset:
-            # First sample or key has changed. Reset the state.
-            check_input["key"] = msg_in.key
-            # Determine if we need to modify the transformed axis.
-            if (
-                axis in msg_in.axes
-                and hasattr(msg_in.axes[axis], "data")
-                and weights.shape[0] != weights.shape[1]
-            ):
-                in_labels = msg_in.axes[axis].data
-                new_labels = []
-                n_in, n_out = weights.shape
-                if len(in_labels) != n_in:
-                    # Something upstream did something it wasn't supposed to. We will drop the labels.
-                    ez.logger.warning(
-                        f"Received {len(in_labels)} for {n_in} inputs. Check upstream labels."
-                    )
-                else:
-                    b_filled_outputs = np.any(weights, axis=0)
-                    b_used_inputs = np.any(weights, axis=1)
-                    if np.all(b_used_inputs) and np.all(b_filled_outputs):
-                        # All inputs are used and all outputs are used, but n_in != n_out.
-                        # Mapping cannot be determined.
-                        new_labels = []
-                    elif np.all(b_used_inputs):
-                        # Strange scenario: New outputs are filled with empty data.
-                        in_ix = 0
-                        new_labels = []
-                        for out_ix in range(n_out):
-                            if b_filled_outputs[out_ix]:
-                                new_labels.append(in_labels[in_ix])
-                                in_ix += 1
-                            else:
-                                new_labels.append("")
-                    elif np.all(b_filled_outputs):
-                        # Transform is dropping some of the inputs.
-                        new_labels = np.array(in_labels)[b_used_inputs]
-                new_axis = replace(msg_in.axes[axis], data=np.array(new_labels))
-
-        data = msg_in.data
-
-        if data.shape[axis_idx] == (weights.shape[0] - 1):
-            # The weights are stacked A|B where A is the transform and B is a single row
-            #  in the equation y = Ax + B. This supports NeuroKey's weights matrices.
-            sample_shape = data.shape[:axis_idx] + (1,) + data.shape[axis_idx + 1 :]
-            data = np.concatenate(
-                (data, np.ones(sample_shape).astype(data.dtype)), axis=axis_idx
-            )
-
-        if axis_idx in [-1, len(msg_in.dims) - 1]:
-            data = np.matmul(data, weights)
-        else:
-            data = np.moveaxis(data, axis_idx, -1)
-            data = np.matmul(data, weights)
-            data = np.moveaxis(data, -1, axis_idx)
-
-        replace_kwargs = {"data": data}
-        if new_axis is not None:
-            replace_kwargs["axes"] = {**msg_in.axes, axis: new_axis}
-        msg_out = replace(msg_in, **replace_kwargs)
+from .base import (
+    GenAxisArray,
+    ProcessorState,
+    BaseStatefulTransformer,
+    BaseTransformerUnit,
+)
 
 
 class AffineTransformSettings(ez.Settings):
@@ -134,21 +25,136 @@ class AffineTransformSettings(ez.Settings):
     """
 
     weights: np.ndarray | str | Path
+    """An array of weights or a path to a file with weights compatible with np.loadtxt."""
+
     axis: str | None = None
+    """The name of the axis to apply the transformation to. Defaults to the leading (0th) axis in the array."""
+
     right_multiply: bool = True
+    """Set False to transpose the weights before applying."""
 
 
-class AffineTransform(GenAxisArray):
-    """:obj:`Unit` for :obj:`affine_transform`"""
+class AffineTransformState(ProcessorState):
+    weights: npt.NDArray | None = None
+    new_axis: AxisBase | None = None
 
+
+class AffineTransformTransformer(
+    BaseStatefulTransformer[AffineTransformSettings, AxisArray, AffineTransformState]
+):
+    def __call__(self, message: AxisArray) -> AxisArray:
+        # Override __call__ so we can shortcut if weights are None.
+        if self.settings.weights is None:
+            return message
+        return super().__call__(message)
+
+    def _hash_message(self, message: AxisArray) -> int:
+        return hash(message.key)
+
+    def _reset_state(self, message: AxisArray) -> None:
+        weights = self.settings.weights
+        if isinstance(weights, str):
+            if weights == "passthrough":
+                weights = None
+            else:
+                weights = Path(os.path.abspath(os.path.expanduser(weights)))
+        if isinstance(weights, Path):
+            weights = np.loadtxt(weights, delimiter=",")
+        if not self.settings.right_multiply:
+            weights = weights.T
+        if weights is not None:
+            weights = np.ascontiguousarray(weights)
+
+        self._state.weights = weights
+
+        axis = self.settings.axis or message.dims[-1]
+        if (
+            axis in message.axes
+            and hasattr(message.axes[axis], "data")
+            and weights.shape[0] != weights.shape[1]
+        ):
+            in_labels = message.axes[axis].data
+            new_labels = []
+            n_in, n_out = weights.shape
+            if len(in_labels) != n_in:
+                ez.logger.warning(
+                    f"Received {len(in_labels)} for {n_in} inputs. Check upstream labels."
+                )
+            else:
+                b_filled_outputs = np.any(weights, axis=0)
+                b_used_inputs = np.any(weights, axis=1)
+                if np.all(b_used_inputs) and np.all(b_filled_outputs):
+                    new_labels = []
+                elif np.all(b_used_inputs):
+                    in_ix = 0
+                    new_labels = []
+                    for out_ix in range(n_out):
+                        if b_filled_outputs[out_ix]:
+                            new_labels.append(in_labels[in_ix])
+                            in_ix += 1
+                        else:
+                            new_labels.append("")
+                elif np.all(b_filled_outputs):
+                    new_labels = np.array(in_labels)[b_used_inputs]
+
+            self._state.new_axis = replace(
+                message.axes[axis], data=np.array(new_labels)
+            )
+
+    def _process(self, message: AxisArray) -> AxisArray:
+        axis = self.settings.axis or message.dims[-1]
+        axis_idx = message.get_axis_idx(axis)
+        data = message.data
+
+        if data.shape[axis_idx] == (self._state.weights.shape[0] - 1):
+            # The weights are stacked A|B where A is the transform and B is a single row
+            #  in the equation y = Ax + B. This supports NeuroKey's weights matrices.
+            sample_shape = data.shape[:axis_idx] + (1,) + data.shape[axis_idx + 1 :]
+            data = np.concatenate(
+                (data, np.ones(sample_shape).astype(data.dtype)), axis=axis_idx
+            )
+
+        if axis_idx in [-1, len(message.dims) - 1]:
+            data = np.matmul(data, self._state.weights)
+        else:
+            data = np.moveaxis(data, axis_idx, -1)
+            data = np.matmul(data, self._state.weights)
+            data = np.moveaxis(data, -1, axis_idx)
+
+        replace_kwargs = {"data": data}
+        if self._state.new_axis is not None:
+            replace_kwargs["axes"] = {**message.axes, axis: self._state.new_axis}
+
+        return replace(message, **replace_kwargs)
+
+
+class AffineTransform(
+    BaseTransformerUnit[AffineTransformSettings, AxisArray, AffineTransformTransformer]
+):
     SETTINGS = AffineTransformSettings
 
-    def construct_generator(self):
-        self.STATE.gen = affine_transform(
-            weights=self.SETTINGS.weights,
-            axis=self.SETTINGS.axis,
-            right_multiply=self.SETTINGS.right_multiply,
+
+def affine_transform(
+    weights: np.ndarray | str | Path,
+    axis: str | None = None,
+    right_multiply: bool = True,
+) -> AffineTransformTransformer:
+    """
+    Perform affine transformations on streaming data.
+
+    Args:
+        weights: An array of weights or a path to a file with weights compatible with np.loadtxt.
+        axis: The name of the axis to apply the transformation to. Defaults to the leading (0th) axis in the array.
+        right_multiply: Set False to transpose the weights before applying.
+
+    Returns:
+        :obj:`AffineTransformTransformer`.
+    """
+    return AffineTransformTransformer(
+        AffineTransformSettings(
+            weights=weights, axis=axis, right_multiply=right_multiply
         )
+    )
 
 
 def zeros_for_noop(data: npt.NDArray, **ignore_kwargs) -> npt.NDArray:
