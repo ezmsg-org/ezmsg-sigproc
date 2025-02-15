1f233f908fa9c2696f82ba96911c37eee96e3ced
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-02-15 10:40:57 -0500

    ruff format

--- src/ezmsg/sigproc/base.py
+++ src/ezmsg/sigproc/base.py
@@ -320,7 +320,9 @@ class BaseAdaptiveTransformer(
     @abstractmethod
     def partial_fit(self, message: SampleMessage) -> None: ...
 
-    def __call__(self, message: typing.Union[MessageType, SampleMessage]) -> typing.Optional[MessageType]:
+    def __call__(
+        self, message: typing.Union[MessageType, SampleMessage]
+    ) -> typing.Optional[MessageType]:
         """
         Adapt transformer with training data (and optionally labels)
         in SampleMessage
