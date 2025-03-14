48da79477e802fea002cc2e9ca4394d9de1f86e1
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-03-14 00:10:14 -0400

    ruff

--- src/ezmsg/sigproc/diff.py
+++ src/ezmsg/sigproc/diff.py
@@ -1,7 +1,11 @@
 import ezmsg.core as ez
 import numpy as np
 import numpy.typing as npt
-from ezmsg.sigproc.base import BaseTransformerUnit, processor_state, BaseStatefulTransformer
+from ezmsg.sigproc.base import (
+    BaseTransformerUnit,
+    processor_state,
+    BaseStatefulTransformer,
+)
 from ezmsg.util.messages.axisarray import AxisArray, slice_along_axis
 from ezmsg.util.messages.util import replace
 
@@ -17,12 +21,14 @@ class DiffState:
     last_time: float | None = None
 
 
-class DiffTransformer(BaseStatefulTransformer[DiffSettings, AxisArray, AxisArray, DiffState]):
+class DiffTransformer(
+    BaseStatefulTransformer[DiffSettings, AxisArray, AxisArray, DiffState]
+):
     def _hash_message(self, message: AxisArray) -> int:
         ax_idx = message.get_axis_idx(self.settings.axis)
         sample_shape = message.data.shape[:ax_idx] + message.data.shape[ax_idx + 1 :]
         return hash((sample_shape, message.key))
-    
+
     def _reset_state(self, message):
         ax_idx = message.get_axis_idx(self.settings.axis)
         self.state.last_dat = slice_along_axis(message.data, slice(0, 1), axis=ax_idx)
@@ -33,16 +39,19 @@ class DiffTransformer(BaseStatefulTransformer[DiffSettings, AxisArray, AxisArray
                     self.state.last_time = 2 * ax_info.data[0] - ax_info.data[1]
                 else:
                     self.state.last_time = ax_info.data[0] - 0.001
-    
+
     def _process(self, message: AxisArray) -> AxisArray:
         axis = self.settings.axis or message.dims[0]
         ax_idx = message.get_axis_idx(axis)
-        
+
         diffs = np.diff(
-            np.concatenate((self.state.last_dat, message.data), axis=ax_idx), axis=ax_idx
+            np.concatenate((self.state.last_dat, message.data), axis=ax_idx),
+            axis=ax_idx,
         )
         # Prepare last_dat for next iteration
-        self.state.last_dat = slice_along_axis(message.data, slice(-1, None), axis=ax_idx)
+        self.state.last_dat = slice_along_axis(
+            message.data, slice(-1, None), axis=ax_idx
+        )
         # Scale by fs if requested. This convers the diff to a derivative. e.g., diff of position becomes velocity.
         if self.settings.scale_by_fs:
             ax_info = message.get_axis(axis)
@@ -68,7 +77,5 @@ class Downsample(
     SETTINGS = DiffSettings
 
 
-def diff(
-    axis: str = "time", scale_by_fs: bool = False
-) -> DiffTransformer:
+def diff(axis: str = "time", scale_by_fs: bool = False) -> DiffTransformer:
     return DiffTransformer(DiffSettings(axis=axis, scale_by_fs=scale_by_fs))
