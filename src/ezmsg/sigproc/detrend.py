22b199f3a62559a405fdfb34fce2b6e5cd2290b6
Chadwick Boulay <chadwick.boulay@gmail.com>
2026-02-06 16:16:04 -0500

    Override on_settings in DetrendUnit.

--- src/ezmsg/sigproc/detrend.py
+++ src/ezmsg/sigproc/detrend.py
@@ -1,3 +1,4 @@
+import ezmsg.core as ez
 import scipy.signal as sps
 from ezmsg.baseproc import BaseTransformerUnit
 from ezmsg.util.messages.axisarray import AxisArray, replace
@@ -35,3 +36,22 @@ class DetrendTransformer(EWMATransformer):
 
 class DetrendUnit(BaseTransformerUnit[EWMASettings, AxisArray, AxisArray, DetrendTransformer]):
     SETTINGS = EWMASettings
+
+    @ez.subscriber(BaseTransformerUnit.INPUT_SETTINGS)
+    async def on_settings(self, msg: EWMASettings) -> None:
+        """
+        Handle settings updates with smart reset behavior.
+
+        Only resets state if `axis` changes (structural change).
+        Changes to `time_constant` or `accumulate` are applied without
+        resetting accumulated state.
+        """
+        old_axis = self.SETTINGS.axis
+        self.apply_settings(msg)
+
+        if msg.axis != old_axis:
+            # Axis changed - need full reset
+            self.create_processor()
+        else:
+            # Only accumulate or time_constant changed - keep state
+            self.processor.settings = msg
